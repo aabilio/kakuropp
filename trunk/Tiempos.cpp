fe23cd05ddf270a483c3fc648a01ae5fedd3a7c8
aabilio@gmail.com <aabilio@gmail.com@f0ddc78f-5d07-66b4-24d5-4091dcd44f8d>
2012-06-11 22:40:47 +0000

    AproximaciOn a Tiempos. No tiene por que ser esto, solo estaba jugando..

--- trunk/Tiempos.cpp
+++ trunk/Tiempos.cpp
@@ -3,3 +3,48 @@
 Tiempos::Tiempos()
 {
 }
+
+Tiempos::Tiempos(char *name, int l): level(l)
+{
+    sprintf(this->name, "%s", name);
+}
+
+void Tiempos::setName(char *name)
+{
+    sprintf(this->name, "%s", name);
+}
+
+char * Tiempos::getName(void)
+{
+    char *tmp;
+    tmp = new char[MAX_NAME];
+    sprintf(tmp, "%s", this->name);
+    return tmp;
+}
+
+void Tiempos::saveScores(void)
+{
+    this->file.open(FILE, MODE);
+    this->file << this->name << ";" << this->time << ";" << this->level << endl;
+    this->file.close();
+}
+
+RegistroRef Tiempos::loadScores(void)
+{
+    char **tmp;
+    string line;
+    RegistroRef reg;
+
+    tmp = new char * [MAX_TAM];
+    for (int i=0; i<MAX_TAM; i++) tmp[i] = new char[MAX_TAM];
+
+    this->file.open(FILE, MODE);
+
+    reg = new Registro;
+    for (int i=0; getline(this->file, line); i++)
+    {
+        //reg->level = line.
+    }
+
+}
+
--- trunk/Tiempos.h
+++ trunk/Tiempos.h
@@ -1,10 +1,39 @@
 #ifndef TIEMPOS_H
 #define TIEMPOS_H
 
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+#define FILE "tiempos.sav"
+#define MODE fstream::in | fstream::out | fstream::app | fstream::ate
+#define MAX_TAM 256
+#define MAX_NAME 32
+
+typedef struct
+{
+    char *nombre[MAX_NAME];
+    int time;
+    int level;
+} Registro;
+
+typedef Registro * RegistroRef;
+
 class Tiempos
 {
+    fstream file;
+    char name[MAX_NAME];
+    int time;
+    int level;
+
 public:
     Tiempos();
+    Tiempos(char *name, int level);
+    void setName(char *name);
+    char * getName(void);
+    void saveScores(void);
+    RegistroRef loadScores(void);
 };
 
 #endif // TIEMPOS_H
